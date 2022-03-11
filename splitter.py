36af0ed364b83bc25760a016a508f08fe81ea62e
Alessandro Bernocchi <60480768+bernagit@users.noreply.github.com>
2022-03-11 13:42:26 +0100

    add vertical page splitter

---
+++ splitter.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from pdfrw import PdfReader, PdfWriter, PageMerge
+
+
+def splitpage(src):
+    '''Split a page into two (top and bottom)'''
+    for y_pos in (0, 0.5):
+        print(y_pos)
+        page = PageMerge()
+        page.add(src, viewrect=(0, y_pos, 1, 0.5))
+        page[0].y = page[0].h/15
+        yield page.render()
+
+print("Inserisci il nome del file: ")
+inpfn = input()
+outfn = 'splitv.' + os.path.basename(inpfn)
+writer = PdfWriter()
+for page in PdfReader(inpfn).pages:
+    writer.addpages(splitpage(page))
+writer.write(outfn)

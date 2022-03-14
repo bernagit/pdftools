684cf2c0f25e30a1130fb3975539f99a98010624
Alessandro Bernocchi <60480768+bernagit@users.noreply.github.com>
2022-03-14 12:01:17 +0100

    splitter works with argument

--- splitter.py
+++ splitter.py
@@ -6,14 +6,14 @@ from pdfrw import PdfReader, PdfWriter, PageMerge
 def splitpage(src):
     '''Split a page into two (top and bottom)'''
     for y_pos in (0, 0.5):
-        print(y_pos)
+        #print(y_pos)
         page = PageMerge()
         page.add(src, viewrect=(0, y_pos, 1, 0.5))
         page[0].y = page[0].h/15
         yield page.render()
 
-print("Inserisci il nome del file: ")
-inpfn = input()
+inpfn = sys.argv[1]
+print("splitting ..... ", inpfn)
 outfn = 'splitv.' + os.path.basename(inpfn)
 writer = PdfWriter()
 for page in PdfReader(inpfn).pages:
